212b88589b3c753c96c8bc828033bbdfd3275ec1
rhoboro <rhoboro@gmail.com>
2019-02-22 15:41:57 +0900

    GCEのベストプラクティスはスコープはフルオープンでIAM制御なのでそれに合わせる

--- gce_task_runner/gce.py
+++ gce_task_runner/gce.py
@@ -141,14 +141,7 @@ class Client:
                 {
                     "email": "default",
                     "scopes": [
-                        "https://www.googleapis.com/auth/sqlservice.admin",
-                        "https://www.googleapis.com/auth/servicecontrol",
-                        "https://www.googleapis.com/auth/service.management.readonly",
-                        "https://www.googleapis.com/auth/logging.write",
-                        "https://www.googleapis.com/auth/monitoring.write",
-                        "https://www.googleapis.com/auth/trace.append",
-                        "https://www.googleapis.com/auth/devstorage.read_only",
-                        "https://www.googleapis.com/auth/pubsub"
+                        "https://www.googleapis.com/auth/cloud-platform"
                     ]
                 }
             ],
