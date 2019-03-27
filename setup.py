59ff402b2b395dc666c6c08f548f754a04d08aa7
rhoboro <rhoboro@gmail.com>
2019-03-27 15:16:01 +0900

    サンプルを更新

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def main():
     setup(
         name='gce-task-runner',
-        version='1.0.0',
+        version='1.1.0',
         author='rei.suyama',
         author_email='rei.suyama@sensy.ai',
         maintainer='rei.suyama',
